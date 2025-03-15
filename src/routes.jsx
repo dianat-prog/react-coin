c17ec30074a37f411b29f4c38c9be72e6bd64b3e
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-03-15 19:05:06 +0100

    añado routes

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
+import Home from "./components/home/Home";
+import Coin from "./components/coin/Coin";
+import Favorites from "./components/favorites/favorites";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "coin/:id", element: <Coin /> },
+     { path: "favorites", element: <Favorites /> },
+    ],
+  },
+]);
+
+export default router;
